abe972bfae514de70c4e8c5a6844e296a924a81b
Peter Halliday <pghalliday@gmail.com>
2012-11-09 10:42:01 +0100

    initial import

---
+++ Cache/Test/OOOTests.h
@@ -0,0 +1,1 @@
+#include "OOOTestCache.Test.h"
---
+++ Cache/src/OOOTestCache.h
@@ -0,0 +1,17 @@
+#ifndef OOOTestCache_H
+#define OOOTestCache_H
+
+#include "OOOCode.h"
+#include "OOOICache.h"
+
+#define OOOClass OOOTestCache
+OOODeclare()
+	OOOImplements
+		OOOImplement(OOOICache)
+	OOOImplementsEnd
+	OOOExports
+	OOOExportsEnd
+OOODeclareEnd
+#undef OOOClass
+
+#endif
